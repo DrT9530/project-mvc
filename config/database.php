aa6c608764520abbe86e4dd4187176e1489bbd26
DrT <athallahrisky@gmail.com>
2025-11-01 02:51:07 +0700

    pls yang terakhir

--- config/database.php
+++ config/database.php
@@ -5,6 +5,6 @@ return [
     'port' => getenv('DB_PORT') ?: '3306',
     'dbname' => getenv('DB_NAME') ?: 'mvc_db',
     'username' => getenv('DB_USER') ?: 'root',
-    'password' => getenv('DB_PASSWORD') ?: '',
+    'password' => getenv('DB_PASSWORD') ?: 'athallah0905',
     'charset' => 'utf8mb4'
 ];
